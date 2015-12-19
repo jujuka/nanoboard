2e72c0f13515c5485322993ea6b5408df7558aa6
Anonymous <nanoboard@openmailbox.org>
2015-12-19 18:26:56 +0200

    Refactor

---
+++ Server/Handlers/ErrorHandler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace nboard
+{
+    class ErrorHandler : IRequestHandler
+    {
+        private readonly string _statusLine;
+        private readonly string _description;
+
+        public ErrorHandler(string statusLine, string description)
+        {
+            _statusLine = statusLine;
+            _description = description;
+        }
+
+        public NanoHttpResponse Handle(NanoHttpRequest request)
+        {
+            return new NanoHttpResponse(
+                _statusLine, 
+                (_statusLine.ToHeader(2) + _description.ToPar()).ToHtmlBody());
+        }
+    }
+    
+}
