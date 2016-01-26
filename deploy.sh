441ef9e9ded4254acff1ea279376d08172a7f15f
Anonymous <nanoboard@openmailbox.org>
2016-01-27 00:23:37 +0200

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
 zip nboard_release.zip bin/Release/nboard.exe bin/Release/places.txt bin/Release/styles/*.css bin/Release/categories.txt bin/Release/containers/dummy.png bin/Release/key.txt bin/Release/README.txt
+git add nboard_release.zip
+git commit -m 'Upload new release'
+git push -u origin master
