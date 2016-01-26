b6860c4a9a8879443a5f15dedc06636568d226c5
Anonymous <nanoboard@openmailbox.org>
2016-01-26 23:48:13 +0200

    Resotre download to memory

--- Png/Aggregator.cs
+++ Png/Aggregator.cs
@@ -164,6 +164,7 @@ namespace nboard
                 return;
             _downloaded.Add(address);
 
+
             try
             {
                 File.AppendAllText(Downloaded, address + "\n");
@@ -186,32 +187,41 @@ namespace nboard
             var client = new WebClient();
             client.Headers = _headers;
 
-            try
+            client.DownloadDataCompleted += (object sender, DownloadDataCompletedEventArgs e) => 
             {
-                if (!Directory.Exists("temp"))
-                {
-                    Directory.CreateDirectory("temp");
-                }
-
-                if (!Directory.Exists(Strings.Download))
-                {
-                    Directory.CreateDirectory(Strings.Download);
-                }
-            }
-            catch
-            {
-            }
-
-            string filename = Strings.Download + Path.DirectorySeparatorChar + new Guid().ToString().Trim('{', '}');
-            client.DownloadFileCompleted += (object sender, System.ComponentModel.AsyncCompletedEventArgs e) => {
                 InProgress -= 1;
-                GC.Collect();
+
+                try
+                {
+                    if (!Directory.Exists("temp"))
+                    {
+                        Directory.CreateDirectory("temp");
+                    }
+
+                    if (!Directory.Exists(Strings.Download))
+                    {
+                        Directory.CreateDirectory(Strings.Download);
+                    }
+
+                    var name = Guid.NewGuid().ToString().Trim('{', '}');
+                    File.WriteAllBytes("temp" + Path.DirectorySeparatorChar + name, e.Result);
+                    File.Move("temp" + Path.DirectorySeparatorChar + name, Strings.Download + Path.DirectorySeparatorChar + name);
+                }
+
+                catch (Exception ex)
+                {
+                    Logger.LogErrorDrawLine();
+                    Logger.LogError(address);
+                    if (e.Error != null)
+                        Logger.LogError(e.Error.Message);
+                    Logger.LogError(ex.Message);
+                }
             };
 
             InProgress += 1;
-            address = address.Replace("2ch.hk", "m2-ch.ru"); // TODO: move to config
+            address = address.Replace("2ch.hk", "m2-ch.ru");
             NotificationHandler.Instance.AddNotification(address);
-            client.DownloadFileAsync(new Uri(address), filename);
+            client.DownloadDataAsync(new Uri(address));
         }
     }
 }
