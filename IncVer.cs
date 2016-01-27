290dcb5a1e66419832e5fe0e9687d71b2cd7fb83
Anonymous <nanoboard@openmailbox.org>
2016-01-28 01:07:32 +0200

    Finish playing with deployment and versioning

---
+++ IncVer.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+
+class Program
+{
+  static void Main() {
+    string vd = "version";
+    string sp = Path.DirectorySeparatorChar.ToString();
+    if (!Directory.Exists(vd)) {
+      Directory.CreateDirectory(vd);
+      File.WriteAllText(vd+sp+"xy", "1.4");
+      File.WriteAllText(vd+sp+"z", "8");
+    }
+    string xy = File.ReadAllText(vd+sp+"xy");
+    int z = int.Parse(File.ReadAllText(vd+sp+"z"));
+    z += 1;
+    File.WriteAllText(vd+sp+"z", z.ToString());
+    File.WriteAllText("App.cs", @"namespace nboard
+{
+  class App
+  {
+    public static string Version = """ + xy + "." + z + @"-dev"";
+  }
+}");
+  }
+}
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+dmcs IncVer.cs
+mono IncVer.exe
+xbuild nboard.csproj /p:Configuration="Release" 
 zip nboard_release.zip bin/Release/nboard.exe bin/Release/places.txt bin/Release/styles/*.css bin/Release/categories.txt bin/Release/containers/dummy.png bin/Release/key.txt bin/Release/README.txt
+git add App.cs
 git add nboard_release.zip
 git commit -m 'Upload new release'
 git push -u origin master
