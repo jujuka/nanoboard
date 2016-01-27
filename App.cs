4d340ad6974f51bbb9d3d29bccaa65c549560d15
Anonymous <nanoboard@openmailbox.org>
2016-01-27 15:00:40 +0200

    Fix multipart parsing in base64 converter

--- App.cs
+++ App.cs
@@ -2,6 +2,6 @@ namespace nboard
 {
     class App
     {
-        public static string Version = "1.4.1-dev";
+        public static string Version = "1.4.2-dev";
     }
 }
--- Server/Handlers/ConvertResultHandler.cs
+++ Server/Handlers/ConvertResultHandler.cs
@@ -23,8 +23,29 @@ namespace nboard
                 int offset = Encoding.ASCII.GetString(request.Connection.Raw).IndexOf("\r\n\r\n");
                 offset = Encoding.ASCII.GetString(request.Connection.Raw).IndexOf("\r\n\r\n", offset + 4);
                 int length = request.Connection.Raw.Length;
+
+                int stepBack = 2; // multipart ending
+                int dashes = 0;
+
+                for (int i = request.Connection.Raw.Length - 1; i > 0; i--)
+                {
+                    stepBack += 1;
+                    byte b = request.Connection.Raw[i];
+                    char c = (char)b;
+
+                    if (c == '-')
+                    {
+                        dashes += 1;
+
+                        if (dashes == 31)
+                        {
+                            break;
+                        }
+                    }
+                }
+
                 sb.Append("[img=");
-                sb.Append(Convert.ToBase64String(request.Connection.Raw, offset + 4, length - offset - 4));
+                sb.Append(Convert.ToBase64String(request.Connection.Raw, offset + 4, length - offset - 4 - stepBack));
                 sb.Append("]");
                 string prep = "";
                 if (sb.Length > 16384) prep = "Превышен лимит в 16384 символа. Такой нанопост будет хуже ретранслироваться другими.\n";
