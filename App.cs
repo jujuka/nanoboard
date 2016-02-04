0a29020edd0887784ca0813ca89abe63094ed939
Anonymous <nanoboard@openmailbox.org>
2016-02-04 02:23:15 +0200

    Remove all files before putting new project

--- App.cs
+++
@@ -1,7 +0,0 @@
-namespace nboard
-{
-  class App
-  {
-    public static string Version = "1.6.6-dev";
-  }
-}
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-dmcs IncVer.cs
-mono IncVer.exe
-xbuild nboard.csproj /p:Configuration="Release" 
-zip nboard_release.zip bin/Release/allowed.txt bin/Release/js/jquery.min.js bin/Release/js/jquery-ui.min.css bin/Release/js/jquery-ui.min.js bin/Release/spamfilter.txt bin/Release/nboard.exe bin/Release/places.txt bin/Release/styles/*.css bin/Release/categories.txt bin/Release/containers/dummy.png bin/Release/key.txt bin/Release/README.txt
-git add App.cs
-git add nboard_release.zip
-git add version
-git commit -m 'Upload new release'
-git push -u origin master
