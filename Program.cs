139643ccab7c79707a4682cc712f7c9c52ee790a
nanoboard <nanoboard@openmailbox.org>
2016-02-23 21:36:37 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace NDB
 {
     class MainClass
     {
+        /*
+            Application's entry point. 
+            Builds server, assigns new DB instance to it and runs it;
+        */
         public static void Main(string[] args)
         {
             var serv = new HttpServerBuilder(new PostDb()).Build();
