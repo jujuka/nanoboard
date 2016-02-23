878ed86faa113b91ccbe9eb9d611af2c7d18b8a8
nanoboard <nanoboard@openmailbox.org>
2016-02-23 21:52:06 +0200

    Update HashCalculator.cs

--- Database/HashCalculator.cs
+++ Database/HashCalculator.cs
@@ -7,6 +7,12 @@ namespace NDB
     {
         public const int HashCrop = 16;
 
+        /*
+            raw string is post's replyTo hash + post's message (concatenated without any separators)
+            and calculates hash of such post (similar to replyTo hash). 
+            The hash is first 16 bytes of SHA-256 of raw string converted to a hexadecimal string (32 characters as result),
+            example hash (not real): 1234567890abcdefgh1234567890abcdefgh
+        */
         public static string Calculate(string raw)
         {
             byte[] bhash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(raw));
