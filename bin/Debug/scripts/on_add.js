34adcd63b428e9a458e2192b2a6d22765ab30252
Anonymous <nanoboard@openmailbox.org>
2016-02-27 20:45:50 +0200

    Fix transport

--- bin/Debug/scripts/on_add.js
+++ bin/Debug/scripts/on_add.js
@@ -1,12 +1,24 @@
 var transportUri = 'http://127.0.0.1:7543';
 
-function recursivelySendParents(post) {
-	post = JSON.parse(post);
-	$.get('../api/get/' + post.replyTo)
-		.done(function(data){
-			$.post(transportUri, data);
-			recursivelySendParents(data);
-		});
+function httpPost(uri, data) {
+  var x = new XMLHttpRequest();
+  x.open('POST', uri);
+  x.send(data);
+}
+
+function recursivelySendParentsArray(post, arr) {
+  if (arr == undefined) arr = [ post ];
+  $.get('../api/get/' + post.replyTo)
+    .done(function(data){
+      data = JSON.parse(data);
+      arr.push(data);
+      recursivelySendParentsArray(data, arr);
+    })
+    .fail(function() {  // 404 - means we've reached the top
+      var str = JSON.stringify(arr);
+      console.log('sending to transport: ' + str);
+      httpPost(transportUri, str);
+    });
 }
 
 /*
@@ -14,6 +26,5 @@ function recursivelySendParents(post) {
 	post - contains string with json of post object in it, where message is base64 encoded
 */
 function onAdd(post) {
-	$.post(transportUri, post);
-	recursivelySendParents(post);
+  recursivelySendParentsArray(post);
 }
