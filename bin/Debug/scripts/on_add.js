685e8edb9d83d69b8051f8dab55d0d795a817765
Anonymous <nanoboard@openmailbox.org>
2016-02-27 19:29:14 +0200

    Send parents of post to transport

--- bin/Debug/scripts/on_add.js
+++ bin/Debug/scripts/on_add.js
@@ -1,7 +1,19 @@
+var transportUri = 'http://127.0.0.1:7543';
+
+function recursivelySendParents(post) {
+	post = JSON.parse(post);
+	$.get('../api/get/' + post.hash)
+		.done(function(data){
+			$.post(transportUri, data);
+			recursivelySendParents(data);
+		});
+}
+
 /*
 	This function is called each time user adds a post through web-interface.
 	post - contains string with json of post object in it, where message is base64 encoded
 */
 function onAdd(post) {
-	$.post('http://127.0.0.1:7543', post);
+	$.post(transportUri, post);
+	recursivelySendParents(post);
 }
